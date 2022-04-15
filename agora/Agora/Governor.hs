19547455188d61fefd433a5efd9d7c2c803a41c5
fanghr <chfanghr@gmail.com>
2022-04-15 12:43:59 +0800

    modify `GovernorRedeemer` and `Governor`
    * add `MutateDatum` redeemer
    * add `datumNFT` to `Governor` so that we can identify the datum
    * Lift `GovernorDatum` and `GovernorRedeemer`

--- agora/Agora/Governor.hs
+++ agora/Agora/Governor.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 {- |
 Module     : Agora.Governor
 Maintainer : emi@haskell.fyi
@@ -18,9 +20,25 @@ module Agora.Governor (
   governorValidator,
 ) where
 
+--------------------------------------------------------------------------------
+
+import GHC.Generics qualified as GHC
+
+--------------------------------------------------------------------------------
+
 import Agora.Proposal (ProposalId, ProposalThresholds)
+
+--------------------------------------------------------------------------------
+
 import Plutarch (popaque)
 import Plutarch.Api.V1 (PMintingPolicy, PValidator)
+
+--------------------------------------------------------------------------------
+
+import Plutus.V1.Ledger.Value (AssetClass)
+import PlutusTx qualified
+
+--------------------------------------------------------------------------------
 
 -- | Datum for the Governor script.
 data GovernorDatum = GovernorDatum
@@ -28,7 +46,10 @@ data GovernorDatum = GovernorDatum
   -- ^ Gets copied over upon creation of a 'Agora.Proposal.ProposalDatum'.
   , nextProposalId :: ProposalId
   -- ^ What tag the next proposal will get upon creating.
-  }
+  } 
+  deriving stock (Show, GHC.Generic)
+
+PlutusTx.makeIsDataIndexed ''GovernorDatum [('GovernorDatum, 0)]
 
 {- | Redeemer for Governor script. The governor has two primary
      responsibilities:
@@ -42,10 +63,23 @@ data GovernorRedeemer
   | -- | Checks that a SINGLE proposal finished correctly,
     --   and allows minting GATs for each effect script.
     MintGATs
+    -- | Allow effects to mutate the datum
+  | MutateDatum
+  deriving stock (Show, GHC.Generic)
+
+PlutusTx.makeIsDataIndexed 
+  ''GovernorRedeemer 
+  [('CreateProposal,0)
+  ,('MintGATs, 1)
+  ,('MutateDatum, 2)
+  ]
 
 -- | Parameters for creating Governor scripts.
 data Governor
-  = Governor
+  = Governor {
+    -- | NFT that identifies the governor datum
+    datumNFT :: AssetClass
+  }
 
 --------------------------------------------------------------------------------
 
